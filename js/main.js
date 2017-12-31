ed6659a3b6f78ea43c9e6b720944bda43abf2738
Katie Angelo <krineangelo@gmail.com>
2017-12-31 15:33:27 -0500

    Completed About/Finished Home

--- js/main.js
+++ js/main.js
@@ -3,6 +3,46 @@
 /* ----------------------------------------
 Main
 ------------------------------------------- */
-$(".carousel-wrapper").slick({
-	dots: true
+
+$('.contact-button').on('click', function () {
+	 window.location.href="contact.html"
+});
+
+$('.read-more').on('click', function () {
+	 window.location.href="grow.html"
+});
+
+
+
+$(window).on('scroll', function () {
+var distanceScrolled = $(window).scrollTop();
+
+console.log('The distance scrolled is: ' + distanceScrolled);
+ if (distanceScrolled>=70) {
+	 $('nav').addClass('nav');
+ }
+});
+$(window).on('scroll', function () {
+var distanceScrolled = $(window).scrollTop();
+
+console.log('The distance scrolled is: ' + distanceScrolled);
+ if (distanceScrolled>=70) {
+	 $('nav li a').addClass('nav-text');
+ }
+});
+$(window).on('scroll', function () {
+var distanceScrolled = $(window).scrollTop();
+
+console.log('The distance scrolled is: ' + distanceScrolled);
+ if (distanceScrolled<=70) {
+	 $('nav').removeClass('nav');
+ }
+});
+$(window).on('scroll', function () {
+var distanceScrolled = $(window).scrollTop();
+
+console.log('The distance scrolled is: ' + distanceScrolled);
+ if (distanceScrolled<=70) {
+	 $('nav li a').removeClass('nav-text');
+ }
 });
